294927712b1f6a6995358f8d72b11b01fc54b7a4
Irs444 <irs786had123@gmail.com>
2024-04-08 11:26:53 +0530

    first commit

---
+++ backend/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+
+const url = "mongodb+srv://irs786had123:irshad@cluster0.82yikc9.mongodb.net/CERL?retryWrites=true&w=majority&appName=Cluster0"
+
+mongoose.connect()
+.then((result) => {
+    console.log("server connected to database");
+}).catch((err) => {
+    console.log(err);
+});
+
+module.exports = mongoose
